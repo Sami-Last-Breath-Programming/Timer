66b40d917e4d0159bf33751fd99d299540d9ad49
Sami <physicskabaap2025@gmail.com>
2026-06-20 18:25:06 +0530

    Tracker With Sound (No Loop)

---
+++ clangFlags.h
@@ -0,0 +1,30 @@
+#ifndef CLANG_FLAGS_H
+#define CLANG_FLAGS_H
+
+// Clangs Flags
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Wimplicit-int-float-conversion"
+#pragma clang diagnostic ignored "-Wmissing-variable-declarations"
+#pragma clang diagnostic ignored "-Wcast-align"
+#pragma clang diagnostic ignored "-Wpadded"
+#pragma clang diagnostic ignored "-Wextra-semi-stmt"
+#pragma clang diagnostic ignored "-Wstring-conversion"
+#pragma clang diagnostic ignored "-Wfloat-equal"
+#pragma clang diagnostic ignored "-Wimplicit-int-conversion"
+#pragma clang diagnostic ignored "-Wimplicit-fallthrough"
+#pragma clang diagnostic ignored "-Wswitch-default"
+#pragma clang diagnostic ignored "-Wcast-qual"
+#pragma clang diagnostic ignored "-Wswitch-enum"
+#pragma clang diagnostic ignored "-Wshorten-64-to-32"
+#pragma clang diagnostic ignored "-Wmissing-prototypes"
+#pragma clang diagnostic ignored "-Wdouble-promotion"
+#pragma clang diagnostic ignored "-Wreserved-macro-identifier"
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+#pragma clang diagnostic ignored "-Wcast-function-type-strict"
+#pragma clang diagnostic ignored "-Wtautological-unsigned-enum-zero-compare"
+#pragma clang diagnostic ignored "-Wbad-function-cast"
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
+#pragma clang diagnostic ignored "-Wreserved-identifier"
+
+#endif
--- timer.c
+++ timer.c
@@ -3,18 +3,59 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-// Clang Flags
+// Clang Setup
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#include "clangFlags.h"
+
+// MiniAudio setup
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
 
 // Error Macros
 #define ERROR_ARGS "Please Enter the hr:min:sec\n"
+#define ENGINE_ALLOCATION_ERROR "Error: Can't Allocate MiniAudio Engine\n"
+#define ERROR_ENGINE_INIT "Error: Can't Init MiniAudio Engine\n"
+#define ERROR_SOUND_INIT "Error: Can't Init Sound from file\n"
 
 // Main Entry
 int main(int argc, char **args)
 {
     bool isTimeout = false;
     int hr, min, sec; 
+
+    ma_result result; 
+    ma_engine *pEngine;
+    ma_sound sound;
+
+    // Allocate Memoery for the MiniAudio Engine
+    pEngine = malloc(sizeof(ma_engine));
+
+    // Handle the Allocations Errors
+    if (pEngine == NULL)
+    {
+        printf(ENGINE_ALLOCATION_ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    // Init the Engine 
+    result = ma_engine_init(NULL, pEngine);
+
+    // Handle Engine Init Errors 
+    if (result != MA_SUCCESS)
+    {
+        printf(ERROR_ENGINE_INIT);
+        exit(EXIT_FAILURE);
+    }
+
+    // Init the Sound from file 
+    result = ma_sound_init_from_file(pEngine, "sound.mp3", 0, NULL, NULL, &sound);
+
+    // Handle Sound Init Errors 
+    if (result != MA_SUCCESS)
+    {
+        printf(ERROR_SOUND_INIT);
+        exit(EXIT_FAILURE);
+    }
 
     // Handle arguments errors
     if (argc < 4)
@@ -56,6 +97,19 @@ int main(int argc, char **args)
         printf("%02i : %02i : %02i\n", hr, min, sec);
 
     }
+
+    // Player the Sound
+    ma_sound_start(&sound);
+
+    // Wait for Enter to Press
+    getchar();
+
+    // Stop Sound
+    ma_sound_stop(&sound);
+
+    // Free the Engine
+    ma_engine_uninit(pEngine);
+
     return EXIT_SUCCESS;
 }
 #pragma clang diagnostic pop
