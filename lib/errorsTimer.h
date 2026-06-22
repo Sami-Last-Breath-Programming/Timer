bbe957f80f5e1c513b66e7da6c2c5a3c51c55a90
Sami <physicskabaap2025@gmail.com>
2026-06-22 18:05:55 +0530

    Stable: Errors Header

---
+++ lib/errorsTimer.h
@@ -0,0 +1,12 @@
+#ifndef ERRORS_TIMER_H
+#define ERRORS_TIMER_H
+
+// Error Macros
+#define ERROR_ARGS "Please Enter the Hr:Min:Sec !\n"
+#define ENGINE_ALLOCATION_ERROR "Error: Can't Allocate MiniAudio Engine !\n"
+#define ERROR_ENGINE_INIT "Error: Can't Init MiniAudio Engine !\n"
+#define ERROR_SOUND_INIT "Error: Can't Init Sound from file !\n"
+#define ERROR_SOUND_START "Error: Can't Start Sound !\n"
+#define ERROR_SOUND_STOP "Error: Can't Stop Sound !\n"
+
+#endif
--- timer.c
+++ timer.c
@@ -10,16 +10,11 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include "lib/miniaudio.h"
 
+// Errors Setup 
+#include "errorsTimer.h"
+
 // Macros
 #define FOREVER 1
-
-// Error Macros
-#define ERROR_ARGS "Please Enter the Hr:Min:Sec !\n"
-#define ENGINE_ALLOCATION_ERROR "Error: Can't Allocate MiniAudio Engine !\n"
-#define ERROR_ENGINE_INIT "Error: Can't Init MiniAudio Engine !\n"
-#define ERROR_SOUND_INIT "Error: Can't Init Sound from file !\n"
-#define ERROR_SOUND_START "Error: Can't Start Sound !\n"
-#define ERROR_SOUND_STOP "Error: Can't Stop Sound !\n"
 
 // Main Entry
 int main(int argc, char **args)
